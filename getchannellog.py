ef24680b3f826b5b8c6bf18b5fe7b4382f504eaf
Daniel Bowder <D.Bowder@gmail.com>
2017-06-30 07:09:44 -0700

    Create getchannellog.py
    Using the Channel ID, you can pull the chat logs from a given channel in slack.
    The API only lets you pull 1000 messages at a time. You could modify the code to iterate through, but you need to pull the latest time stamp from the 1000th message you pulled so that you know where to start grabbing the next 1000.

---
+++ getchannellog.py
@@ -0,0 +1,15 @@
+import slackclient
+import os
+import json
+
+key = os.getenv('SLACK_PEAK')
+sc = slackclient.SlackClient(key)
+userlist = {} # ul is just a name for userlist
+channellist = {}#cl is just a name for channellist
+
+def GetChatLog(channel,latest, count=1000):
+    return sc.api_call('channels.history', channel=channel, latest=latest, count=count)
+
+channelid = 'place id here'
+latesttimestamp = 1480544737.000002
+print(GetChatLog(channelid, latesttimestamp, count=1000))
